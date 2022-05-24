f39a79b0d6acd6afd7eafe8f077d7908c5f743cd
thibault-chausson <chausson.thibault@laposte.net>
2022-05-25 01:09:58 +0200

    admin ok

--- run_server.py
+++ run_server.py
@@ -18,7 +18,7 @@ import sqlite3
 #reqLog = "create table log (id integer primary key autoincrement, userName text, pass text)"
 #reqAdd="insert into log (userName, pass) values ('jean', 'pierre')"
 #reqSuperLog = "create table superLog (id integer primary key autoincrement, userName text, pass text)"
-#reqSuperAdd="insert into superLog (userName, pass) values ('tibo', 'arti')"
+#reqSuperAdd="insert into log (userName, pass) values ('tibo2', 'arti')"
 #cur.execute(reqSuperAdd)
 #conn.commit()
 #conn.close()
@@ -37,6 +37,13 @@ def deconnexion():
     if de1 == "deco":
         return render_template("login.html")
 
+@app.route('/logSuper', methods=['POST', 'GET'])
+def aller():
+    return render_template("logSuperUser.html")
+
+
+
+
 
 database = {'jean': 'pierre', 'christophe': 'voyage', 'dider': 'paul'}
 
@@ -47,15 +54,15 @@ def login():
     pwd = request.form['password']
     conn2 = sqlite3.connect('myDB.db')
     cur2 = conn2.cursor()
-    cur2.execute("SELECT COUNT(pass) FROM log WHERE userName='name1'")
+    cur2.execute("SELECT COUNT(pass) FROM log WHERE userName=?", (name1,))
     rows2 = cur2.fetchall()
     conn2.close()
-    if rows2==0:
+    if rows2[0][0] ==0:
         return render_template('login.html', info='Utilisateur inconnu')
     else:
         conn3 = sqlite3.connect('myDB.db')
         cur3 = conn3.cursor()
-        cur3.execute("SELECT pass FROM log WHERE userName='jean'")
+        cur3.execute("SELECT pass FROM log WHERE userName=?", (name1,))
         rows3 = cur3.fetchall()
         conn3.close()
         if rows3[0][0] != pwd:
@@ -63,10 +70,56 @@ def login():
         else:
             return render_template('home.html', name=name1, code=SECRET_MESSAGE)
 
+@app.route('/ajout', methods=['POST', 'GET'])
+def loginSuper():
+    name1 = request.form['username']
+    pwd = request.form['password']
+    conn2 = sqlite3.connect('myDB.db')
+    cur2 = conn2.cursor()
+    cur2.execute("SELECT COUNT(pass) FROM superLog WHERE userName=?", (name1,))
+    rows2 = cur2.fetchall()
+    conn2.close()
+    if rows2[0][0] == 0:
+        return render_template('logSuperUser.html', info='Utilisateur inconnu')
+    else:
+        conn3 = sqlite3.connect('myDB.db')
+        cur3 = conn3.cursor()
+        cur3.execute("SELECT pass FROM superLog WHERE userName=?", (name1,))
+        rows3 = cur3.fetchall()
+        conn3.close()
 
-#if __name__ == "__main__":
+        if rows3[0][0] != pwd:
+            return render_template('logSuperUser.html', info='Mot de passe invalide')
+        else:
+            return render_template('addUser.html', name=name1, code=SECRET_MESSAGE)
+
+
+@app.route('/addOk', methods=['POST', 'GET'])
+def addSuper():
+    print("laaaa")
+    name1 = request.form['username']
+    pwd = request.form['password']
+
+    conn3 = sqlite3.connect('myDB.db')
+    cur3 = conn3.cursor()
+    cur3.execute("SELECT COUNT(pass) FROM log WHERE userName=?", (name1,))
+    rows3 = cur3.fetchall()
+    conn3.close()
+    if rows3[0][0] != 0:
+        return render_template('addUser.html', info="Deja le meme id")
+    else:
+        conn2 = sqlite3.connect('myDB.db')
+        cur2 = conn2.cursor()
+        cur2.execute("insert into log (userName, pass) values (? , ?)", (name1,pwd))
+        conn2.commit()
+        conn2.close()
+        return render_template("addUser.html", info='Ok')
+
+
+
+if __name__ == "__main__":
     # HTTP version
-    #app.run(debug=True, host="0.0.0.0", port=8081)
+    app.run(debug=True, host="0.0.0.0", port=8081)
     # HTTPS version
     # A compléter  : nécessité de déplacer les bons fichiers vers ce répertoire
     # app.run(debug=True, host="0.0.0.0", port=8081, ssl_context=(SERVER_PUBLIC_KEY_FILENAME, SERVER_PRIVATE_KEY_FILENAME))
